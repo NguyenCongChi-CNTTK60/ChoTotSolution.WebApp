01c071a2c0218eb0b40f4e4c822912e95ae53581
NguyenCongChi-CNTTK60 <chicongnguyen06072001@gmail.com>
2022-03-30 09:20:35 +0700

    Connect git

---
+++ ChoTotSolution.WebApp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ChoTotSolution.WebApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
